d08b740070acd7d5b89d4be8f18b395fbbaa9f36
Sagarnaikg <sagarnaikg00@gmail.com>
2020-12-11 16:15:38 +0530

    project setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import './App.css';
+import React from "react";
 
-import Map from './components/Map'
+import Map from "./components/Map";
 
 function App() {
   return (
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
-import InteractiveMap, { Marker, GeolocateControl, NavigationControl, FullscreenControl, ScaleControl} from "react-map-gl";
-import { Grid } from '@material-ui/core';
-
+import InteractiveMap, {
+  Marker,
+  GeolocateControl,
+  NavigationControl,
+  FullscreenControl,
+  ScaleControl,
+} from "react-map-gl";
+import { Grid } from "@material-ui/core";
 
 import * as parkDate from "../data/users.json";
-import Pin from './Pin';
-import DonorPin from './DonorPin';
-import MainButton from './MainButton';
+import Pin from "./Pin";
+import DonorPin from "./DonorPin";
+import MainButton from "./MainButton";
 
 export default function Map() {
   const [viewport, setViewport] = useState({
@@ -14,109 +19,108 @@ export default function Map() {
     longitude: -75.6903,
     width: "100vw",
     height: "100vh",
-    zoom: 10
+    zoom: 10,
   });
 
   const geolocateStyle = {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
-    margin: 10
+    margin: 10,
   };
 
   const fullscreenControlStyle = {
-    position: 'absolute',
+    position: "absolute",
     top: 36,
     left: 0,
-    padding: '10px'
+    padding: "10px",
   };
 
   const navStyle = {
-    position: 'absolute',
+    position: "absolute",
     top: 72,
     left: 0,
-    padding: '10px'
+    padding: "10px",
   };
 
   const scaleControlStyle = {
-    position: 'absolute',
+    position: "absolute",
     bottom: 36,
     left: 0,
-    padding: '10px'
+    padding: "10px",
   };
 
-  const [markers, setMarkers] = React.useState([])
-  const handleClick = ({lngLat: [longitude, latitude]}) =>{
-      setMarkers(markers => [{longitude, latitude}])
-  }
-
+  const [markers, setMarkers] = React.useState([]);
+  const handleClick = ({ lngLat: [longitude, latitude] }) => {
+    setMarkers((markers) => [{ longitude, latitude }]);
+  };
 
   return (
     <Grid container>
       <InteractiveMap
         {...viewport}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/moreducks/cki8wapi813ij19o19o698881"
-        onViewportChange={viewport => {
+        mapboxApiAccessToken={
+          "pk.eyJ1IjoidGVhbS1kZWNvZGVycyIsImEiOiJja2lrMno5MHEwNXduMnRveW1xbHZmbm9qIn0.vJTY7weN0PdZe8qh9fgcRA"
+        }
+         mapStyle="mapbox://styles/team-decoders/ckik3z2q00l4j17ujgfec17vn"
+        onViewportChange={(viewport) => {
           setViewport(viewport);
         }}
         onClick={handleClick}
-        style={{zIndex:50}}
+        style={{ zIndex: 50 }}
       >
+        {markers.length
+          ? markers.map((m, i) => (
+              <Marker {...m} key={i}>
+                <div
+                  style={{
+                    height: 200,
+                    width: 200,
+                    background:
+                      "radial-gradient(100px circle at center, rgba(255,82,82,0.7) 0%, rgba(0,128,128,0) 100%)",
+                    transform: "translateY(-100px) translateX(-100px)",
+                    zIndex: -1,
+                  }}
+                >
+                  {console.log(
+                    `Clicked here: \n ${m.longitude}, ${m.latitude}`
+                  )}
+                  <Pin />
+                </div>
+              </Marker>
+            ))
+          : null}
 
-      {markers.length? markers.map((m, i) => (
-
-          <Marker {...m} key={i} >
-            <div style=
-              {{
-                height:200,
-                width:200,
-                background:'radial-gradient(100px circle at center, rgba(255,82,82,0.7) 0%, rgba(0,128,128,0) 100%)',
-                transform: 'translateY(-100px) translateX(-100px)',
-                zIndex:-1
-              }}
-            >
-              {console.log(`Clicked here: \n ${m.longitude}, ${m.latitude}`)}
-              <Pin/>
-            </div>
+        {parkDate.features.map((park) => (
+          <Marker
+            key={park.properties.PARK_ID}
+            latitude={park.geometry.coordinates[1]}
+            longitude={park.geometry.coordinates[0]}
+          >
+            <DonorPin />
           </Marker>
+        ))}
 
-        ))
-      : null}
+        <GeolocateControl
+          style={geolocateStyle}
+          positionOptions={{ enableHighAccuracy: true }}
+          trackUserLocation={true}
+        />
 
-      {parkDate.features.map(park => (
-        <Marker
-          key={park.properties.PARK_ID}
-          latitude={park.geometry.coordinates[1]}
-          longitude={park.geometry.coordinates[0]}
-        >
-          <DonorPin/>
-        </Marker>
-      ))}
+        <div style={fullscreenControlStyle}>
+          <FullscreenControl />
+        </div>
 
+        <div style={navStyle}>
+          <NavigationControl />
+        </div>
 
-      <GeolocateControl
-        style={geolocateStyle}
-        positionOptions={{enableHighAccuracy: true}}
-        trackUserLocation={true}
-      />
-
-      <div style={fullscreenControlStyle}>
-        <FullscreenControl />
-      </div>
-
-      <div style={navStyle}>
-        <NavigationControl />
-      </div>
-
-      <div style={scaleControlStyle}>
-        <ScaleControl />
-      </div>
-
+        <div style={scaleControlStyle}>
+          <ScaleControl />
+        </div>
       </InteractiveMap>
 
       <MainButton />
-
     </Grid>
   );
 }
