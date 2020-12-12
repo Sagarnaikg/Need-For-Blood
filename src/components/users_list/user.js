3523e4e1a7ea17a7013e439a70588419036b60f1
Poorvi <poorvistudies@gmail.com>
2020-12-13 01:36:14 +0530

    active users modal

--- src/components/users_list/user.js
+++ src/components/users_list/user.js
@@ -1,11 +1,27 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 import * as users from "../../data/users.json";
 import "./users.css";
 
 export default function User() {
   const usersList = users.features;
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <>
@@ -15,6 +31,8 @@ export default function User() {
 
       <h4 className="subTitle">Accepted</h4>
 
+
+
       {usersList.map((user) => {
         if (
           user.properties.NAME === "Douglas Lawson" ||
@@ -22,7 +40,7 @@ export default function User() {
         ) {
           if (!user.properties.ONLINE) {
             return (
-              <div key={user.properties.PARK_ID} className="user-card">
+              <><div key={user.properties.PARK_ID} className="user-card" >
                 <Grid container spacing={0}>
                   <Grid item xs={2} className="user-img">
                     <img
@@ -40,9 +58,10 @@ export default function User() {
                   </Grid>
                 </Grid>
               </div>
+              </>
             );
           }
-          return (
+          return (<>
             <div key={user.properties.PARK_ID} className="user-card">
               <Grid container spacing={0}>
                 <Grid item xs={2} className="user-img">
@@ -58,9 +77,48 @@ export default function User() {
                   </strong>
                   <p className="user-text-msg">18:00</p>
                   <p className="user-text-msg">Accepted</p>
+                  <Button variant="outlined" size="small" color="primary" onClick={handleClickOpen}>
+                    Details
+                  </Button>
                 </Grid>
               </Grid>
+
+              <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                key={user.properties.PARK_ID}
+              >
+                <DialogTitle id="alert-dialog-title"><strong>Potential Donor</strong></DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                  <Grid container spacing={3}>
+                  <Grid item xs={3}>
+                    <img
+                      src={user.properties.PROFILE}
+                      alt="avatar"
+                      className="user-img-avatar1"
+                      style={{display:'inline'}}
+                    />
+                  </Grid>
+                  <Grid item xs={9}>
+                    <h2 style={{paddingTop:10}}>{user.properties.NAME}</h2>
+                  </Grid>
+                  </Grid>
+                  <Grid container spacing={3}>
+                    <Grid item xs={3}><strong>Address:</strong></Grid><Grid item xs={9}>{user.properties.ADDRESS}</Grid>
+                    <Grid item xs={3}><strong>Email:</strong></Grid><Grid item xs={9}>{user.properties.EMAIL}</Grid>
+                    <Grid item xs={3}><strong>Phone:</strong></Grid><Grid item xs={9}>{user.properties.PHONE}</Grid>
+                  </Grid>
+                  </DialogContentText>
+                </DialogContent>
+              </Dialog>
+              
             </div>
+
+
+            </>
           );
         }
         return <></>;
